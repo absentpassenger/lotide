675a4065f48ca3eeac1cec86c01c4ef18525f83f
faisa fatah <absentcodes@gmail.com>
2023-02-01 01:06:58 +0200

    comparing arrays with eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length  !== arrayTwo.length) {
+    return false;
+  }
+
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (Array.isArray(arrayOne[i]) && (eqArrays(arrayOne[i], arrayTwo[i]) === false)) {
+      return false;
+    }
+
+    if (!(Array.isArray(arrayOne[i])) && arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+
+  }
+  return true 
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+
