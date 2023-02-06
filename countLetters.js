f4e1cafb01635dc854215225d9c8e8167dd3d395
faisa fatah <absentcodes@gmail.com>
2023-02-06 04:56:10 +0200

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+//assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+
+// count letters function
+
+const countLetters = function(input) {
+  const results = {};
+
+  for (const letter of input) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"))
+assertEqual((countLetters("lighthouse")));
